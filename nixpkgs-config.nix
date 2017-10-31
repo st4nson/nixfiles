4f28515f98a4435d08e48bb4779b848ce144bfa9
Stanislaw Szydlo <stanislaw.szydlo@intel.com>
2017-10-31 09:53:48 +0100

    nixprofile config added.

---
+++ nixpkgs-config.nix
@@ -0,0 +1,33 @@
+{
+  packageOverrides = pkgs: with pkgs; rec {
+
+    customVim = pkgs.vim_configurable.override {
+      features = "huge"; # one of  tiny, small, normal, big or huge
+      gui = "gtk";
+      cfg = {
+        luaSupport = true;
+        rubySupport = true;
+        cscopeSupport = true;
+        pythonSupport = true;
+        python3Support = true;
+        multibyteSupport = true;
+      };
+      flags = {
+        xim.enable = true;
+      };
+    };
+
+    myPackages = pkgs.buildEnv {
+      name = "myEnv";
+      paths = [
+        customVim
+        (python27.withPackages(ps: with ps; [
+          jinja2
+          pyyaml
+          cryptography
+        ]))
+      ];
+    };
+
+  };
+}
