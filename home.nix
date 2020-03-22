cc92dc01407400f1723c03e94d7d494d92c223e8
Stanislaw Szydlo <stanislaw.szydlo@intel.com>
2020-03-22 19:00:00 +0100

    Move local pkgs to home.nix. 1-1 for now.

--- home.nix
+++ home.nix
@@ -4,7 +4,70 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.packages = [
+  home.packages = with pkgs; [
+    ansible-lint
+    ansible_2_7
+    cfssl
+    gopass
+    openconnect
+    redshift
+    taskwarrior
+
+    docker-compose
+    docker-ls
+    docker-machine
+
+    chromium
+    firefox
+    icedtea8_web
+    keepassxc
+    libreoffice
+    pidgin-with-plugins
+    remmina
+    shutter
+    slack
+
+    ctags
+    go_1_13
+    nodejs
+    shellcheck
+    yarn
+
+    kubectx
+    qemu
+    virtmanager
+
+    ranger
+    restic
+    terraform-lsp
+
+    # Python
+    pipenv
+    (python3.withPackages(ps: with ps; [
+      isort
+      jedi
+      jinja2
+      pylint
+      pep8
+      flake8
+      pyopenssl
+      pytest
+      pycodestyle
+      pyyaml
+      requests
+      tox
+      xmltodict
+    ]))
+    # TODO
+    #python3Packages.black
+    #python3Packages.nose
+    #python3Packages.pyflakes
+    #python3Packages.pygments
+    #python3Packages.pyls-black
+    #python3Packages.pyls-isort
+    #python3Packages.pytest
+    #python3Packages.python-language-server
+
   ];
 
   programs.zsh = {
