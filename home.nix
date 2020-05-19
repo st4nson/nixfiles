3e6668e8a56939a96d56f2b79f968490fe04f994
Stanisław Szydło <stanislaw.szydlo@intel.com>
2020-05-19 18:19:49 +0200

    Add AwesomeWM config.

--- home.nix
+++ home.nix
@@ -165,6 +165,25 @@
     tmuxinator.enable = true;
   };
 
+  xsession = {
+    enable = true;
+    windowManager.awesome.enable = true;
+  };
+  home.file.".config/awesome/rc.lua".source = ./files/awesome/rc.lua;
+
+  home.file.".config/awesome".recursive = true;
+  home.file.".config/awesome".source = ./files/awesome/awesome-copycats;
+
+  home.file.".config/awesome/lain".recursive = true;
+  home.file.".config/awesome/lain".source = ./files/awesome/lain;
+
+  home.file.".config/awesome/freedesktop".recursive = true;
+  home.file.".config/awesome/freedesktop".source = ./files/awesome/freedesktop;
+  home.file.".config/awesome/pomodoro".recursive = true;
+  home.file.".config/awesome/pomodoro".source = ./files/awesome/pomodoro;
+
+  home.file.".config/awesome/themes/st4nson".recursive = true;
+  home.file.".config/awesome/themes/st4nson".source = ./files/awesome/themes/st4nson;
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
