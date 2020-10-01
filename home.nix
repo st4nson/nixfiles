0e6f761a0700cdba626df236e22b344cf4ad65c2
Stanisław Szydło <stanislaw.szydlo@nike.com>
2020-10-01 13:18:12 +0200

    refactor: conditional 'awesome' config

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@ in
 {
   imports =
     [
+      ./home/awesome.nix
       ./home/nvim.nix
       ./home/tmux.nix
       ./home/zsh.nix
@@ -148,27 +149,6 @@ in
   };
 
   home.file.".alacritty.yml".source = ./files/alacritty.yml;
-
-  # AwesomeWM configuration
-  xsession = {
-    enable = true;
-    windowManager.awesome.enable = true;
-  };
-  home.file.".config/awesome/rc.lua".source = ./files/awesome/rc.lua;
-
-  home.file.".config/awesome".recursive = true;
-  home.file.".config/awesome".source = ./files/awesome/awesome-copycats;
-
-  home.file.".config/awesome/lain".recursive = true;
-  home.file.".config/awesome/lain".source = ./files/awesome/lain;
-
-  home.file.".config/awesome/freedesktop".recursive = true;
-  home.file.".config/awesome/freedesktop".source = ./files/awesome/freedesktop;
-  home.file.".config/awesome/pomodoro".recursive = true;
-  home.file.".config/awesome/pomodoro".source = ./files/awesome/pomodoro;
-
-  home.file.".config/awesome/themes/st4nson".recursive = true;
-  home.file.".config/awesome/themes/st4nson".source = ./files/awesome/themes/st4nson;
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
