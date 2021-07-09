9d6dfab3d0392bbd8d08b3fc7d4c2022a7f6fb67
Stanisław Szydło <stanislaw.szydlo@nike.com>
2021-07-09 14:49:33 +0200

    fix: pin neovim ~0.5.0 + vimwiki colors

--- home.nix
+++ home.nix
@@ -22,7 +22,7 @@ in
 
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/neovim-nightly-overlay/archive/2aff1c00dc619b5b8af3dee95461b41f3a6fd7f0.tar.gz;
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/b8619f5196d6257b147722b394d3dadb3f5eaa9a.tar.gz; # ~0.5.0
     }))
   ];
 
