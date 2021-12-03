82c503c0b5b1861b72a8702104a4f7e3e254f739
Stanisław Szydło <stanislaw.szydlo@nike.com>
2021-12-03 17:49:57 +0100

    fix: update flake input to 'release-21.11'

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "st4nson's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-21.11-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
     #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-21.05-darwin";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
