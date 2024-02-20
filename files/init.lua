e6cbc7a523b932a346000194234dcc8b180bd54a
Stanisław Szydło <st4nson@gmail.com>
2024-02-20 16:04:14 +0100

    refactor: bigger one
    - switch 'vim-go' to 'go.nvim'
    - switch 'fzf' to 'telescope'
    - 'cmp' improvements

--- files/init.lua
+++ files/init.lua
@@ -58,24 +58,6 @@ vim.api.nvim_set_keymap("n", "<leader>[", ":bprevious<CR>", { noremap = true, si
 vim.api.nvim_set_keymap("n", "<leader>]", ":bnext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>;", ":BD<CR>", { noremap = true, silent = true })
 
--- fzf
-vim.api.nvim_set_keymap("n", "<C-p>", ":FZF<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>'", ":Buffers<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>q", ":Tags<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>=", ":History<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>se", ":Ag<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>gs", ":GFiles?<CR>", { noremap = true, silent = true })
-
--- run fzf in floating window
-vim.g.fzf_layout = {
-	window = {
-		width = 0.9,
-		height = 0.8,
-		highlight = 'FloatermBorder',
-		border = 'rounded'
-	}
-}
-
 -- vim-floaterm
 vim.g.floaterm_autoclose = 1 -- close window on success
 vim.g.floaterm_autohide = 2  -- always autohide
@@ -131,54 +113,54 @@ vim.g.slime_default_config = {
 -- vim-go
 
 -- run :GoBuild or :GoTestCompile based on the go file
-local function go_build()
-  local fname = vim.fn.expand("%:p:h") .. "/" .. vim.fn.expand("%:p:r")
-  local go_build = "go build " .. fname .. ".go"
-  local go_test = "go test " .. fname .. "_test.go"
-  if vim.fn.filereadable(go_build) == 1 then
-	vim.cmd(":!go build " .. fname .. ".go")
-  elseif vim.fn.filereadable(go_test) == 1 then
-	vim.cmd(":!go test " .. fname .. "_test.go")
-  else
-	vim.cmd(":GoBuild")
-  end
-end
+--local function go_build()
+  --local fname = vim.fn.expand("%:p:h") .. "/" .. vim.fn.expand("%:p:r")
+  --local go_build = "go build " .. fname .. ".go"
+  --local go_test = "go test " .. fname .. "_test.go"
+  --if vim.fn.filereadable(go_build) == 1 then
+	--vim.cmd(":!go build " .. fname .. ".go")
+  --elseif vim.fn.filereadable(go_test) == 1 then
+	--vim.cmd(":!go test " .. fname .. "_test.go")
+  --else
+	--vim.cmd(":GoBuild")
+  --end
+--end
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "go",
-  callback = function(args)
-	vim.keymap.set('n', '<leader>b', function() go_build() end, { noremap = true })
-  end
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "go",
-  callback = function(args)
-	vim.keymap.set('n', '<leader>r', '<Plug>(go-run)', { noremap = true })
-  end
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "go",
-  callback = function(args)
-	vim.keymap.set('n', '<leader>t', '<Plug>(go-test)', { noremap = true })
-  end
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "go",
-  callback = function(args)
-	vim.keymap.set('n', '<leader>c', '<Plug>(go-coverage-toggle)', { noremap = true })
-  end
-})
+--vim.api.nvim_create_autocmd("FileType", {
+  --pattern = "go",
+  --callback = function(args)
+	--vim.keymap.set('n', '<leader>b', function() go_build() end, { noremap = true })
+  --end
+--})
+--vim.api.nvim_create_autocmd("FileType", {
+  --pattern = "go",
+  --callback = function(args)
+	--vim.keymap.set('n', '<leader>r', '<Plug>(go-run)', { noremap = true })
+  --end
+--})
+--vim.api.nvim_create_autocmd("FileType", {
+  --pattern = "go",
+  --callback = function(args)
+	--vim.keymap.set('n', '<leader>t', '<Plug>(go-test)', { noremap = true })
+  --end
+--})
+--vim.api.nvim_create_autocmd("FileType", {
+  --pattern = "go",
+  --callback = function(args)
+	--vim.keymap.set('n', '<leader>c', '<Plug>(go-coverage-toggle)', { noremap = true })
+  --end
+--})
 
 
-vim.g.go_list_type = "quickfix"
-vim.g.go_fmt_command = "goimports"
+--vim.g.go_list_type = "quickfix"
+--vim.g.go_fmt_command = "goimports"
 
-vim.g.go_gopls_enabled = 1
-vim.g.go_gopls_options = {'-remote=auto'}
+--vim.g.go_gopls_enabled = 1
+--vim.g.go_gopls_options = {'-remote=auto'}
 
-vim.g.go_code_completion_enabled = 0
-vim.g.go_doc_keywordprg_enabled = 0
-vim.g.go_def_mapping_enabled = 0
+--vim.g.go_code_completion_enabled = 0
+--vim.g.go_doc_keywordprg_enabled = 0
+--vim.g.go_def_mapping_enabled = 0
 
 -- colorizer
 require'colorizer'.setup()
@@ -226,8 +208,16 @@ require('telescope').setup{
   }
 }
 
--- nvim-lspfuzzy
-require('lspfuzzy').setup{}
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', "<C-p>", telescope_builtin.find_files, { noremap = true, silent = true })
+vim.keymap.set('n', "<leader>'", telescope_builtin.buffers, { noremap = true, silent = true })
+vim.keymap.set('n', "<leader>q", telescope_builtin.tags, { noremap = true, silent = true })
+vim.keymap.set('n', "<leader>=", telescope_builtin.oldfiles, { noremap = true, silent = true })
+vim.keymap.set('n', "<leader>se", telescope_builtin.live_grep, { noremap = true, silent = true })
+vim.keymap.set('n', "<leader>gs", telescope_builtin.git_status, { noremap = true, silent = true })
+
+local trouble = require('trouble')
+vim.keymap.set("n", "<leader>t", trouble.toggle, { noremap = true, silent = true })
 
 -- nvim-cmp
 local lspkind = require "lspkind"
@@ -246,13 +236,15 @@ cmp.setup({
 
   sources = {
     { name = 'nvim_lsp' },
-    { name = 'path' },
     { name = 'ultisnips' }, -- For ultisnips users.
+    { name = 'path' },
     { name = 'buffer', keyword_length = 3 },
-    { name = 'treesitter' },
+    --{ name = 'treesitter' },
     { name = 'emoji' }
   },
 
+  completion = { completeopt = 'menu,menuone,noinsert,noselect' },
+  preselect = cmp.PreselectMode.None,
   formatting = {
     -- Youtube: How to set up nice formatting for your sources.
     format = lspkind.cmp_format {
@@ -310,48 +302,47 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- vim.lsp.set_log_level("debug")
 local nvim_lspconfig = require('lspconfig')
 
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
--- Use an on_attach function to only map the following keys
+-- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    --vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-require('lsp_signature').on_attach(lsp_signature_cfg)
-
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
-  -- workspaces
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-
-end
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-
-local lsp_default_config = {on_attach = on_attach, capabilties = capabilties}
+local lsp_default_config = {capabilties = capabilties}
 local servers = {
   jsonls = {},
   yamlls = {
@@ -487,3 +478,42 @@ require('gitsigns').setup{
     map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end
 }
+-- go.nvim
+require('go').setup()
+
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
+
+-- noice.nvim
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = false, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = true, -- add a border to hover docs and signature help
+  },
+  views = {
+    cmdline_popup = {
+      position = {
+        row = "35%",
+        col = "50%",
+      },
+    },
+  },
+})
--- home/nvim.nix
+++ home/nvim.nix
@@ -1,41 +1,5 @@
 { pkgs, ... }:
 let
-  vim-delve-custom = pkgs.vimUtils.buildVimPlugin {
-    name = "vim-delve";
-    src = pkgs.fetchFromGitHub {
-      owner = "sebdah";
-      repo = "vim-delve";
-      rev = "554b7997caba5d2b38bc4a092e3a468e4abb7f18";
-      sha256 = "11qsvw3qsrkwmdks6mhmygmwzi9ma8vhx77kid5s7p936i8xdmxr";
-    };
-  };
-  nvim-lspfuzzy-custom = pkgs.vimUtils.buildVimPlugin {
-    name = "nvim-lspfuzzy";
-    src = pkgs.fetchFromGitHub {
-      owner = "ojroques";
-      repo = "nvim-lspfuzzy";
-      rev = "9ad50ac644a438799dc452dfeeed9437aa5aa8b6";
-      sha256 = "6m1ekW0LDDDj/XduNH7+JEFxw3QeeabVf251WOnEgzA=";
-    };
-  };
-  lsp-signature-custom = pkgs.vimUtils.buildVimPlugin {
-    name = "lsp_signature.nvim";
-    src = pkgs.fetchFromGitHub {
-      owner = "ray-x";
-      repo = "lsp_signature.nvim";
-      rev = "9ccee20602a10843e3ea3ebc2536dfdcc6cee9a3";
-      sha256 = "";
-    };
-  };
-  fzf-floaterm-custom = pkgs.vimUtils.buildVimPlugin {
-    name = "fzf-floaterm";
-    src = pkgs.fetchFromGitHub {
-      owner = "voldikss";
-      repo = "fzf-floaterm";
-      rev = "66a30db85a7adf573af9b8a4f3f8c4ce0a2d665e";
-      sha256 = "0p9grgi8hybgg7341ffn4y812axx552ncrhlak5cka3k0zpil06g";
-    };
-  };
   cmp-jenkinsfile-custom = pkgs.vimUtils.buildVimPlugin {
     name = "cmp-jenkinsfile";
     src = pkgs.fetchFromGitHub {
@@ -43,16 +7,6 @@ let
       repo = "cmp-jenkinsfile";
       rev = "f06750822f9b2eb282ba8321d47d0a833968f6dd";
       sha256 = "gx75kAnkl8WVthKfWueCIjkwOTPqAZLXCK4J5En861I=";
-    };
-  };
-  cmp-nvim-ultisnips-custom = pkgs.vimUtils.buildVimPlugin {
-    name = "cmp-nvim-ultisnips";
-    buildPhase = "echo hello";
-    src = pkgs.fetchFromGitHub {
-      owner = "quangnguyen30192";
-      repo = "cmp-nvim-ultisnips";
-      rev = "79fd645096c406fb41b38ef4dd99525965b446b1";
-      sha256 = "7Hu7H5Q7XgqgMFkFLRkwqDen6535h83KnsCHsfd1yas=";
     };
   };
 in
@@ -71,6 +25,9 @@ in
 
       telescope-nvim
       trouble-nvim
+      noice-nvim
+      nui-nvim
+      nvim-notify
 
       # completion & lsp
       nvim-cmp
@@ -80,6 +37,7 @@ in
       cmp-jenkinsfile-custom
       cmp-nvim-lsp
       cmp-nvim-lsp-document-symbol
+      cmp-nvim-lsp-signature-help
       cmp-nvim-lua
       cmp-nvim-ultisnips
       cmp-path
@@ -89,7 +47,7 @@ in
       lsp-colors-nvim
       lspkind-nvim
       lsp_signature-nvim
-      nvim-lspfuzzy-custom
+      goto-preview
 
       # git
       vim-fugitive
@@ -103,8 +61,6 @@ in
       catppuccin-nvim
 
       # motion
-      fzf-vim
-      fzf-floaterm-custom
       nerdcommenter
       nvim-tree-lua
       vim-bufkill
@@ -115,8 +71,7 @@ in
 
       # programming
       Jenkinsfile-vim-syntax
-      vim-delve-custom
-      vim-go
+      go-nvim
       vim-nix
       vim-terraform
       vim-test
