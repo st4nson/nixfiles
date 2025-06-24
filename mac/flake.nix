5313ded613ff95e8bef04f64829e00f82ffa74f2
Stanisław Szydło <stanislaw.szydlo@nike.com>
2025-06-24 14:08:12 +0200

    chore: bump to 25.05

--- mac/flake.nix
+++ mac/flake.nix
@@ -2,11 +2,11 @@
   description = "st4nson's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
-    darwin.url = "github:lnl7/nix-darwin/nix-darwin-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    darwin.url = "github:lnl7/nix-darwin/nix-darwin-25.05";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
